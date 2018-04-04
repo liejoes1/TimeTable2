08573c0fb47619310cbbde04a3064afab4e90e31
liejoes1 <liejoes1@gmail.com>
2018-04-04 19:51:50 +0800

    Fix Bug Error on R.

---
+++ app/src/main/java/com/example/time/ontime/MainActivity.java
@@ -0,0 +1,201 @@
+package com.example.time.ontime;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.example.time.ontime.Settings.SettingsActivity;
+import com.example.time.ontime.TimeTable.FragmentPager;
+import com.example.time.ontime.TimeTable.RecyclerAdapter;
+import com.example.time.ontime.TimeTable.TimeTableFragment;
+
+
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+
+    public RecyclerAdapter mAdapter;
+    public static LinearLayout NoClassLinearLayout, LoadingScreenLinearLayout, NoInternetLinearLayout;
+    public static RelativeLayout DisplayClassRelativeLayout;
+
+    public static boolean INTAKE_CHANGED;
+    public static boolean NO_INTERNET = false;
+
+    public static String INTAKE_STATUS;
+
+    public static void noInternt() {
+        NO_INTERNET = true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        this.getSupportActionBar().show();
+        //Provide Context
+        TimeTableFragment.getContext(getApplicationContext());
+
+        if (NO_INTERNET) {
+            NoInternetLinearLayout.setVisibility(View.VISIBLE);
+            LoadingScreenLinearLayout.setVisibility(View.GONE);
+            DisplayClassRelativeLayout.setVisibility(View.GONE);
+            NoClassLinearLayout.setVisibility(View.GONE);
+        } else {
+
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            String value = sharedPreferences.getString("intakestatus", "");
+            Log.i("TAH", "Status: " + value);
+            if (value.equals("SUCCESS")) {
+                LoadingScreenLinearLayout.setVisibility(View.GONE);
+                DisplayClassRelativeLayout.setVisibility(View.VISIBLE);
+                NoClassLinearLayout.setVisibility(View.GONE);
+            } else if (value.equals("FAILED")) {
+                LoadingScreenLinearLayout.setVisibility(View.GONE);
+                DisplayClassRelativeLayout.setVisibility(View.GONE);
+                NoClassLinearLayout.setVisibility(View.VISIBLE);
+
+            }
+
+            if (INTAKE_CHANGED) {
+                showSnackbar(getWindow().getDecorView().findViewById(android.R.id.content), "Intake changed successfully", Snackbar.LENGTH_LONG);
+                INTAKE_CHANGED = false;
+            }
+            ViewPager viewPager = findViewById(R.id.viewpager);
+            FragmentPager adapter = new FragmentPager(getSupportFragmentManager());
+            viewPager.setAdapter(adapter);
+            viewPager.setOffscreenPageLimit(5);
+
+            TabLayout tabLayout = findViewById(R.id.tab_layout);
+            tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+
+
+    }
+
+    public void showSnackbar(View view, String message, int duration) {
+        // Create snackbar
+        final Snackbar snackbar = Snackbar.make(view, message, duration);
+
+
+        // Set an action on it, and a handler
+        snackbar.setAction("Refresh", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupSharedPreference();
+                INTAKE_CHANGED = false;
+            }
+        });
+
+        snackbar.show();
+    }
+
+    private void setupSharedPreference() {
+        NO_INTERNET = false;
+
+        this.getSupportActionBar().hide();
+        NoClassLinearLayout.setVisibility(View.GONE);
+        DisplayClassRelativeLayout.setVisibility(View.GONE);
+        LoadingScreenLinearLayout.setVisibility(View.VISIBLE);
+        if (NetworkActivity.isNetworkAvailable(this)) {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            new NetworkActivity.GetTimeTableInfo().execute(sharedPreferences.getString(getString(R.string.pref_timetable_key), getString(R.string.pref_timetable_default)));
+            sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+        } else {
+            NO_INTERNET = true;
+            Intent startMainActivity = new Intent(this, MainActivity.class);
+            startActivity(startMainActivity);
+
+        }
+
+    }
+
+
+    private void init() {
+        NoClassLinearLayout = (LinearLayout) findViewById(R.id.ll_no_class);
+        LoadingScreenLinearLayout = (LinearLayout) findViewById(R.id.ll_loading_screen);
+        DisplayClassRelativeLayout = (RelativeLayout) findViewById(R.id.rl_display_data);
+
+        NoInternetLinearLayout = (LinearLayout) findViewById(R.id.ll_no_internet);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int IDGathered = item.getItemId();
+        if (IDGathered == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        } else if (IDGathered == R.id.action_refresh) {
+            setupSharedPreference();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        INTAKE_CHANGED = true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    public static boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            Intent exitAppIntent = new Intent(Intent.ACTION_MAIN);
+            exitAppIntent.addCategory(Intent.CATEGORY_HOME);
+            startActivity(exitAppIntent);
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit.", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
+    }
+}
---
+++ app/src/main/java/com/example/time/ontime/NetworkActivity.java
@@ -0,0 +1,256 @@
+package com.example.time.ontime;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+
+import com.example.time.ontime.Utils.DataParsing;
+import com.example.time.ontime.Utils.Decompress;
+import com.example.time.ontime.Utils.Utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Joes on 20/3/2018.
+ */
+
+public class NetworkActivity {
+
+    private static Context appContext;
+    public static String ROOT_DIRECTORY_PATH;
+    private static String ROOT_TEMP_PATH;
+
+    private static final String TIMETABLE_LIST_URL = "https://webspace.apiit.edu.my/intake-timetable/TimetableIntakeList/TimetableIntakeList.xml";
+    private static String TIMETABLE_INFO_BASE = "https://webspace.apiit.edu.my/intake-timetable/replyLink.php?stid=";
+
+    public static boolean INITIAL_APP_LAUNCH;
+
+    public static void setContext(Context context) {
+        appContext = context;
+        CreateFolder();
+    }
+
+    public static void setInitialAppLaunch(boolean initialAppLaunch) {
+        INITIAL_APP_LAUNCH = initialAppLaunch;
+    }
+
+    public static class GetTimeTableList extends AsyncTask<Void, Void, Void> {
+
+        File TempFile = new File(ROOT_TEMP_PATH, "TimeTableListTemporary.xml");
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                DownloadFile(new URL(TIMETABLE_LIST_URL), TempFile);
+                //File Successfully Downloaded to the provided Folder
+                //After download, we can start copy to the root to be read.
+                //Nothing to be passed here
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            //After creating file copy the content now.
+            CopyData(new File(ROOT_DIRECTORY_PATH, "TimeTableList.xml"), TempFile);
+            //When Done Pass the file to be processed
+            try {
+                //Parse Timetable List
+                DataParsing.ParseTimeTableList(new FileInputStream(new File(ROOT_DIRECTORY_PATH, "TimeTableList.xml")));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class GetTimeTableInfo extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder fullString = new StringBuilder();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(TIMETABLE_INFO_BASE + strings[0]).openStream()));
+                String tempStr;
+
+                while ((tempStr = bufferedReader.readLine()) != null) {
+                    fullString.append(tempStr);
+                }
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return fullString.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+            if (s.equals("Time table for the current week is not available for this intake code.")) {
+
+                SharedPreferences.Editor editor = sharedPreferences.edit().putString("intakestatus", "FAILED");
+                editor.apply();
+
+                //When error, don't downlaod the timetable, cause not exist, instead go to Main Activity and show error
+                Intent MainActivityIntent = new Intent(appContext, MainActivity.class);
+                appContext.startActivity(MainActivityIntent);
+
+            } else {
+                SharedPreferences.Editor editor = sharedPreferences.edit().putString("intakestatus", "SUCCESS");
+                editor.apply();
+
+                new GetTimeTableData().execute(s);
+            }
+        }
+
+    }
+
+    public static class GetTimeTableData extends AsyncTask<String, Void, Void> {
+
+        File TempFile = new File(ROOT_TEMP_PATH, "TimeTableTemporary.zip");
+
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            try {
+                DownloadFile(new URL(strings[0]), TempFile);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            //Unzip the Data
+            new Decompress(TempFile.toString(), ROOT_TEMP_PATH + "/TimeTableTemporary/").unzip();
+
+            //Copy the Data to the root
+            CopyData(new File(ROOT_DIRECTORY_PATH, "TimeTable.xml"), new File(ROOT_TEMP_PATH + "/TimeTableTemporary/" + "timetable.xml"));
+
+            //Parse the data{
+            try {
+                DataParsing.ParseTimeTable(new FileInputStream(new File(ROOT_DIRECTORY_PATH, "TimeTable.xml")));
+
+                Intent MainActivityIntent = new Intent(appContext, MainActivity.class);
+                appContext.startActivity(MainActivityIntent);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+/***********************************************************************************************************************************************************/
+/*
+Limit Here, Don't change anything below,
+*/
+
+    private static void CreateFolder() {
+        ROOT_DIRECTORY_PATH = Utils.getRootDirPath(appContext);
+        File TempDir = new File(ROOT_DIRECTORY_PATH, "/temp");
+        if (!TempDir.exists()) {
+            TempDir.mkdirs();
+        }
+        ROOT_TEMP_PATH = TempDir.toString();
+    }
+
+    private static void DownloadFile(URL url, File TempFile) {
+        try {
+            HttpURLConnection httpURLConnection = getNetworkData(url);
+            FileOutputStream fileOutputStream = new FileOutputStream(TempFile);
+            InputStream inputStream = httpURLConnection.getInputStream();
+            int lengthOfFile = httpURLConnection.getContentLength();
+
+            byte data[] = new byte[1024];
+            int count = 0;
+            long total = 0;
+            int progress = 0;
+
+            while ((count = inputStream.read(data)) != -1) {
+                total += count;
+                int progress_temp = (int) total * 100 / lengthOfFile;
+                if (progress_temp % 10 == 0 && progress != progress_temp) {
+                    progress = progress_temp;
+                }
+                fileOutputStream.write(data, 0, count);
+            }
+            inputStream.close();
+            fileOutputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void CopyData(File NewFile, File OldFile) {
+        try {
+            InputStream inputStream = new FileInputStream(OldFile);
+            OutputStream outputStream = new FileOutputStream(NewFile);
+
+            byte[] bytes = new byte[1024];
+            int lengthOfFile;
+            while ((lengthOfFile = inputStream.read(bytes)) > 0) {
+                outputStream.write(bytes, 0, lengthOfFile);
+            }
+            inputStream.close();
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static HttpURLConnection getNetworkData(URL url) {
+        HttpURLConnection httpURLConnection = null;
+        try {
+            httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            httpURLConnection.setRequestMethod("GET");
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(15000);
+            httpURLConnection.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return httpURLConnection;
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
